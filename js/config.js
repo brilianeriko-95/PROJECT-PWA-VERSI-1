aa72337e7835e5d44403e0f10deb14ac53ad30d3
brilianeriko-95 <brilianeriko95@gmail.com>
2026-04-10 01:00:23 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,7 +5,7 @@
 // ============================================
 // 1. APP CONFIGURATION
 // ============================================
-const APP_VERSION = '2.8.2';
+const APP_VERSION = '2.8.3';
 const APP_NAME = 'PROJECT LOGSHEET';
 
 const AUTH_CONFIG = {
